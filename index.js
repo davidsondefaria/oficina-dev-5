c5ba3001deab199670e2138918017f7bbb4c501c
Davidson de Faria <davidsondefaria@gmail.com>
2025-07-16 18:11:45 -0300

    feat: acessing huddle from user chat page

--- index.js
+++ index.js
@@ -15,18 +15,50 @@ async function main() {
   // logar com email usando o link abaixo antes de tentar pegar a transcrição
   // await page.goto("https://slack.com/signin");
 
-  await page.goto("https://app.slack.com/huddle/T02HDPYV6J3/C02HDPZ0W6B");
+  await page.goto("https://app.slack.com/client/T09137B6UNN/D0912TB66NQ");
 
-  const popupPromise = page.waitForEvent("popup", { timeout: 500000 });
+  await page.getByLabel("Start huddle with davidsondefaria").click();
+
+  console.log("started huddle");
+
+  // Set up popup listener before clicking
+  const popupPromise = page.waitForEvent("popup", { timeout: 10000 });
+
+  console.log("Clicking 'Open huddle window'...");
+  await page.getByLabel("Open huddle window").click();
+
   const popup = await popupPromise;
 
   const txt = popup.locator(huddleTitle);
 
   console.log("Huddle title: ", await txt.textContent());
 
+  let transcriptionActivated = false;
+
   while (true) {
     const msgLocator = popup.locator(".p-huddle_event_log__transcription");
     console.log("Message visible");
+
+    if (!transcriptionActivated) {
+      await popup.getByLabel("Settings").click();
+      console.log("Settings clicked");
+
+      await popup.getByText("Show captions").click();
+      console.log("Show captions clicked");
+
+      await popup.getByText("Side-by-side").click();
+      console.log("Side-by-side clicked");
+
+      await popup.getByText("Change huddle language").click();
+      console.log("Change huddle language clicked");
+
+      await popup.getByText("Português (Brasil)").click();
+      console.log("Language changed to Português (Brasil)");
+
+      await popup.getByText("Continue").click();
+      console.log("Continue clicked");
+      transcriptionActivated = true;
+    }
 
     try {
       const msg = await msgLocator.textContent({ timeout: 5000000 });
