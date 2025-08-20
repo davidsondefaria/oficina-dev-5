31342a1ec07d4ba9631fa47295e31bc80a2f08c5
Davidson de Faria <davidsondefaria@gmail.com>
2025-08-20 17:41:37 -0300

    feat: adicionando mutações para pegar a última mensagem

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 const { chromium } = require("playwright");
+const { setTimeout } = require("node:timers/promises");
 // const { huddleTitle } = require("./selectors");
+
+const handleTranscriptionUpdate = (popup) => {
+  return async (text) => {
+    try {
+      const msgName = popup.locator(
+        ".c-virtual_list__item[tabindex='0'] .p-huddle_event_log__member_name"
+      );
+      console.log("User: ", await msgName.textContent());
+
+      const msgText = popup.locator(
+        ".c-virtual_list__item[tabindex='0'] .p-huddle_event_log__transcription"
+      );
+      console.log("Message: ", await msgText.textContent());
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
 
 async function main() {
   const browser = await chromium.launchPersistentContext("/tmp/test-slackbot", {
@@ -29,52 +48,70 @@ async function main() {
 
   const popup = await popupPromise;
 
-  // const txt = popup.locator(huddleTitle);
-
-  // console.log("Huddle title: ", await txt.textContent());
-
+  await popup.exposeFunction(
+    "onTranscriptionUpdate",
+    handleTranscriptionUpdate(popup)
+  );
   let transcriptionActivated = false;
 
   const messages = [];
+  const transcriptSelector = ".c-virtual_list__scroll_container";
 
-  while (true) {
-    // usar uma api do navegador para escutar quando o elemento mudar: mutation observer
+  const mutationObserverInit = ([selector]) => {
+    const targetNode = document.querySelector(selector);
 
-    if (!transcriptionActivated) {
-      await popup.getByLabel("Settings").click();
-      console.log("Settings clicked");
-
-      await popup.getByText("Show captions").click();
-      console.log("Show captions clicked");
-
-      await popup.getByText("Side-by-side").click();
-      console.log("Side-by-side clicked");
-
-      // await popup.getByText("Change huddle language").click();
-      // console.log("Change huddle language clicked");
-
-      // await popup.getByText("Português (Brasil)").click();
-      // console.log("Language changed to Português (Brasil)");
-
-      // await popup.getByText("Continue").click();
-      // console.log("Continue clicked");
-      transcriptionActivated = true;
+    if (!targetNode) {
+      console.error("Elemento da transcrição não encontrado!");
+      return;
     }
 
-    try {
-      const msgName = popup.locator(
-        ".c-virtual_list__item[tabindex='0'] .p-huddle_event_log__member_name"
-      );
-      console.log("User: ", await msgName.textContent());
+    const config = {
+      childList: true,
+      subtree: true,
+      characterData: true,
+    };
 
-      const msgText = popup.locator(
-        ".c-virtual_list__item[tabindex='0'] .p-huddle_event_log__transcription"
-      );
-      console.log("Message: ", await msgText.textContent());
-    } catch (error) {
-      console.log(error);
-    }
+    const callback = (mutationsList, observer) => {
+      console.log("dentro do callback");
+      for (const mutation of mutationsList) {
+        if (mutation.type === "childList") {
+          console.log("A child node has been added or removed.");
+        } else if (mutation.type === "attributes") {
+          console.log(`The ${mutation.attributeName} attribute was modified.`);
+        }
+      }
+      const fullText = targetNode.innerText;
+      window.onTranscriptionUpdate(fullText);
+    };
+
+    const observer = new MutationObserver(callback);
+    observer.observe(targetNode, config);
+
+    window.onTranscriptionUpdate(targetNode.innerText);
+  };
+
+  if (!transcriptionActivated) {
+    await popup.getByLabel("Settings").click();
+    console.log("Settings clicked");
+
+    await popup.getByText("Show captions").click();
+    console.log("Show captions clicked");
+
+    await popup.getByText("Side-by-side").click();
+    console.log("Side-by-side clicked");
+
+    // await popup.getByText("Change huddle language").click();
+    // console.log("Change huddle language clicked");
+
+    // await popup.getByText("Português (Brasil)").click();
+    // console.log("Language changed to Português (Brasil)");
+
+    // await popup.getByText("Continue").click();
+    // console.log("Continue clicked");
+    transcriptionActivated = true;
   }
+  await setTimeout(30_000, null);
+  await popup.evaluate(mutationObserverInit, [transcriptSelector]);
 }
 
 main();
