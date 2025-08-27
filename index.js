c01f1b5ed65320d282d8f8e32f38ea351488ab15
Davidson de Faria <davidsondefaria@gmail.com>
2025-08-27 17:15:37 -0300

    feat: pegando a última mensagem completo e a atual em andamento

--- index.js
+++ index.js
@@ -2,18 +2,56 @@ const { chromium } = require("playwright");
 const { setTimeout } = require("node:timers/promises");
 // const { huddleTitle } = require("./selectors");
 
-const handleTranscriptionUpdate = (popup) => {
+const handleTranscriptionUpdate = (popup, messages) => {
   return async (text) => {
     try {
-      const msgName = popup.locator(
-        ".c-virtual_list__item[tabindex='0'] .p-huddle_event_log__member_name"
-      );
-      console.log("User: ", await msgName.textContent());
+      const msgPai = popup.locator(".c-virtual_list__item[tabindex='0']");
+      const paiId = await msgPai.getAttribute("data-item-key");
 
-      const msgText = popup.locator(
-        ".c-virtual_list__item[tabindex='0'] .p-huddle_event_log__transcription"
+      if (paiId <= 1) return;
+
+      const lastCompletedUser = popup.locator(
+        `.c-virtual_list__item[data-item-key='${
+          paiId - 1
+        }'] .p-huddle_event_log__member_name`
       );
-      console.log("Message: ", await msgText.textContent());
+
+      const lastCompletedMessage = popup.locator(
+        `.c-virtual_list__item[data-item-key='${
+          paiId - 1
+        }'] .p-huddle_event_log__transcription`
+      );
+
+      const currentUser = popup.locator(
+        `.c-virtual_list__item[data-item-key='${paiId}'] .p-huddle_event_log__member_name`
+      );
+
+      const currentMessage = popup.locator(
+        `.c-virtual_list__item[data-item-key='${paiId}'] .p-huddle_event_log__transcription`
+      );
+
+      const messageToUpdate = messages.find((msg) => msg.id === "last");
+
+      if (messageToUpdate) {
+        messageToUpdate.user = await currentUser.textContent();
+        messageToUpdate.message = await currentMessage.textContent();
+      } else {
+        messages.push({
+          user: await currentUser.textContent(),
+          message: await currentMessage.textContent(),
+          id: "last",
+        });
+      }
+
+      if (messages.find((msg) => msg.id === paiId - 1)) return;
+
+      messages.push({
+        user: await lastCompletedUser.textContent(),
+        message: await lastCompletedMessage.textContent(),
+        id: paiId - 1,
+      });
+
+      console.log(messages);
     } catch (error) {
       console.log(error);
     }
@@ -48,13 +86,13 @@ async function main() {
 
   const popup = await popupPromise;
 
+  const messages = [];
   await popup.exposeFunction(
     "onTranscriptionUpdate",
-    handleTranscriptionUpdate(popup)
+    handleTranscriptionUpdate(popup, messages)
   );
   let transcriptionActivated = false;
 
-  const messages = [];
   const transcriptSelector = ".c-virtual_list__scroll_container";
 
   const mutationObserverInit = ([selector]) => {
